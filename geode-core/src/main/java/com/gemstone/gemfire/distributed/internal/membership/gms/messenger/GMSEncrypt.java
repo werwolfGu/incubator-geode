7737c85d7d878690a1a7c2680fbd95c0c6c55ff1
Hitesh Khamesra <hiteshk25@yahoo.com>
2016-06-03 14:57:37 -0700

    GEODE-1372 added test for different algo. Fixed issue for algo without size

--- geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/membership/gms/messenger/GMSEncrypt.java
+++ geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/membership/gms/messenger/GMSEncrypt.java
@@ -84,7 +84,7 @@ public class GMSEncrypt implements Cloneable {
   protected byte[] getClusterSecretKey() {
     return this.clusterEncryptor.secretBytes;
   }
-
+  
   protected synchronized void initClusterSecretKey() throws Exception {
     if(this.clusterEncryptor == null) {
       this.clusterEncryptor = new ClusterEncryptor(this);
@@ -96,7 +96,7 @@ public class GMSEncrypt implements Cloneable {
     //TODO we are reseeting here, in case there is some race
     this.clusterEncryptor = new ClusterEncryptor(secretBytes);
   }
-
+  
   protected GMSEncrypt() {
     initEncryptors();
   }
@@ -230,7 +230,7 @@ public class GMSEncrypt implements Cloneable {
 
   private Map<InternalDistributedMember, PeerEncryptor> getPeerEncryptorMap() {
     int h = Math.abs(Thread.currentThread().getName().hashCode() % numberOfPeerEncryptorCopies);
-    ConcurrentHashMap m = copyOfPeerEncryptors[h];
+    ConcurrentHashMap<InternalDistributedMember, PeerEncryptor> m = copyOfPeerEncryptors[h];
     
     if(m == null) {
       synchronized (copyOfPeerEncryptors) {
@@ -417,10 +417,9 @@ public class GMSEncrypt implements Cloneable {
     int blocksize = getBlockSize(dhSKAlgo);
 
     if (keysize == -1 || blocksize == -1) {
-      // TODO how should we do here, should we just throw runtime exception?
-      /* SecretKey sKey = ka.generateSecret(dhSKAlgo);
-       * encrypt = Cipher.getInstance(dhSKAlgo);
-       * encrypt.init(Cipher.ENCRYPT_MODE, sKey); */
+      SecretKeySpec sks = new SecretKeySpec(secretBytes, dhSKAlgo);
+      encrypt = Cipher.getInstance(dhSKAlgo);
+      encrypt.init(Cipher.ENCRYPT_MODE, sks);
     } else {
       String dhAlgoStr = getDhAlgoStr(dhSKAlgo);
 
@@ -469,10 +468,9 @@ public class GMSEncrypt implements Cloneable {
     int blocksize = getBlockSize(dhSKAlgo);
 
     if (keysize == -1 || blocksize == -1) {
-      // TODO: how to do here, should we just throw runtime exception?
-      /* SecretKey sKey = ka.generateSecret(dhSKAlgo);
-       * decrypt = Cipher.getInstance(dhSKAlgo);
-       * decrypt.init(Cipher.DECRYPT_MODE, sKey); */
+      SecretKeySpec sks = new SecretKeySpec(secretBytes, dhSKAlgo);
+      decrypt = Cipher.getInstance(dhSKAlgo);
+      decrypt.init(Cipher.DECRYPT_MODE, sks);
     } else {
       String algoStr = getDhAlgoStr(dhSKAlgo);
 
@@ -494,7 +492,7 @@ public class GMSEncrypt implements Cloneable {
     int blocksize = getBlockSize(dhSKAlgo);
 
     if (keysize == -1 || blocksize == -1) {
-      SecretKey sKey = ka.generateSecret(dhSKAlgo);
+      SecretKey sKey = ka.generateSecret(dhSKAlgo);      
       return sKey.getEncoded();
     } else {
       return ka.generateSecret();
@@ -532,7 +530,7 @@ public class GMSEncrypt implements Cloneable {
     public ClusterEncryptor(byte[] sb) {
       this.secretBytes = sb;
     }
-
+    
     public synchronized byte[] encryptBytes(byte[] data) throws Exception {
       String algo = dhSKAlgo;
       return GMSEncrypt.encryptBytes(data, getEncryptCipher(algo));
